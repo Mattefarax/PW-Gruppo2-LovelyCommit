be363a751963bd5c694a1593b48889f60ed9d803
Mattefarax <matteo.faraoni@stud.tecnicosuperiorekennedy.it>
2022-07-01 11:05:19 +0200

    Add function to send queue message

--- RPI/send.py
+++ RPI/send.py
@@ -1,3 +1,4 @@
+import json
 import pika
 def send_to_queue(list):
 
@@ -5,13 +6,8 @@ def send_to_queue(list):
     channel = connection.channel()
 
     channel.queue_declare(queue='hello')
-    count=len(list)
-    while(count>0):
-
-        channel.basic_publish(exchange='', routing_key='hello', body=list[count-1]) #Mettendo un byte alla volta, provare a inserire tutti il necessario
-        print(" [x] Sent 'Hello World!'")
-        count-=1
+    message=json.dumps(list)
+    channel.basic_publish(exchange='', routing_key='hello', body=message) #Mettendo un byte alla volta, provare a inserire tutti il necessario
+    print(" [x] Sent 'Hello World!'")
     connection.close()
-    # #channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-    # print(" [x] Sent 'Hello World!'")
-    # connection.close()
+ 
