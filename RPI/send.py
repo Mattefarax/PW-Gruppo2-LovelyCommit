b94b767652b7d7f7c7aa437051f93ef1642df41b
Mattefarax <matteo.faraoni@stud.tecnicosuperiorekennedy.it>
2022-07-04 19:00:14 +0200

    Add multi queue sender function

--- RPI/send.py
+++ RPI/send.py
@@ -1,13 +1,13 @@
 import json
 import pika
-def send_to_queue(list):
+def send_to_queue(list,queue):
 
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue=queue)
     message=json.dumps(list)
-    channel.basic_publish(exchange='', routing_key='hello', body=message) #Mettendo un byte alla volta, provare a inserire tutti il necessario
+    channel.basic_publish(exchange='', routing_key=queue, body=message) #Mettendo un byte alla volta, provare a inserire tutti il necessario
     print(" [x] Sent 'Hello World!'")
     connection.close()
  
