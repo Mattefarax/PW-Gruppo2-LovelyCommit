a8eed7942d579824d65a08066edbbe7b455d57c8
Mattefarax <matteo.faraoni@stud.tecnicosuperiorekennedy.it>
2022-07-04 19:02:14 +0200

    Add async function callback

--- RPI/iotHub_invioReported.py
+++ RPI/iotHub_invioReported.py
@@ -1,26 +1,31 @@
 import os
 import asyncio
 import random
+import pika
 from azure.iot.device.aio import IoTHubDeviceClient
 
 
-async def main():
+def main(message):
+    asyncall(message)
+
+
+    
+
+async def asyncall(message):
     os.environ["IOTHUB_DEVICE_CONNECTION_STRING"] = "HostName=LovelyCommitIotHub.azure-devices.net;DeviceId=Rpi-train1;SharedAccessKey=dcvdrkfRKc1r5RaD/f4k+2EqUE8Kow8YA1Mynzqi8oU="
 
     conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
-
-    # connect the client.
+# connect the client.
     await device_client.connect()
 
     # update the reported properties
-    reported_properties = {"temperature": random.randint(320, 800) / 10}
+    reported_properties = message
     print("Setting reported temperature to {}".format(reported_properties["temperature"]))
     await device_client.patch_twin_reported_properties(reported_properties)
 
     # Finally, shut down the client
     await device_client.shutdown()
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(main())
